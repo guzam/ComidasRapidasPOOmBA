a2f377d5a20d4c97535ec05cc79fd836b23f65fc
Lucas <lucasmarques6@hotmail.com>
2022-06-06 22:36:25 -0300

    Subo clase Usuario incompleta

---
+++ Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComidasRapidasPOOmBA
+{
+    class Usuario
+    {
+        string Nombre;
+        int idUsuario;
+        static int idUsuarioGeneral;
+        string tipoUsuario;
+        string email;
+        string password;
+        bool ingresoPermitido;
+
+        public Usuario(string nom, int idUs, )
+        {
+
+        }
+
+
+    }
+}
