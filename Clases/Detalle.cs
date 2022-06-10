2f67fd674f326bd51e61189c8208f60d5967522d
federicobalbuena <you@example.com>
2022-06-09 22:32:14 -0300

    Sigo subiendo

---
+++ Clases/Detalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComidasRapidasPOOmBA.Clases
+{
+    public class Detalle
+    {
+        DateTime fecha;
+        int cantidad;
+        Producto item;
+
+        public void cantidadXProducto()
+        {
+
+        }
+    }
+}
---
+++ Clases/Ticket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComidasRapidasPOOmBA.Clases
+{
+    public class Ticket
+    {
+        DateTime fecha;
+        int idTicket;
+        string nombreLocal;
+        string afip; //condicion del cliente frente a afip? podria llamarse condicionAfip?
+
+
+        public int IdTicket
+        {
+            get
+            {
+                return idTicket;
+            }
+            set
+            {
+                idTicket = value;
+            }
+        }
+        public string Afip
+        {
+            get
+            {
+                return afip;
+            }
+            set
+            {
+                afip = value;
+            }
+        }
+        public Ticket()
+        {
+            fecha = DateTime.Now;
+
+            nombreLocal = "Mc POOmBA";
+            afip = "";
+        }
+        /*
+        public void imprimir()
+        {
+
+        }
+        */
+
+    }
+}
