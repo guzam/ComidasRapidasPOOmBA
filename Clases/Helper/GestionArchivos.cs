29eb4a9803ef92e70d3459b18c58da36de6b9bbc
Gustavo Z <gustavozammataro@gmail.com>
2022-07-02 11:06:57 -0300

    gestion de pago

---
+++ Clases/Helper/GestionArchivos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComidasRapidasPOOmBA.Clases.Helper
+{
+    public class GestionArchivos
+    {
+        string cabeceraIngreso, pieIngreso;
+        string ingreso;
+
+        public GestionArchivos() { }
+
+        public void escribir(string ruta, string ingreso, bool sobreEscribir)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(ruta,sobreEscribir);
+
+                sw.WriteLine(cabecera());
+                sw.WriteLine(ingreso);
+                sw.WriteLine(pie());
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+        }
+
+        public string cabecera()
+        {
+            return cabeceraIngreso = "Ingreso: " + DateTime.Now.ToString() ;
+        }
+
+        public string pie()
+        {
+            return pieIngreso = "------------------------------------------";
+        }
+    }
+}
